a8cf1439520025246fd503c516083192e13754a0
Manav010203 <manav01022003@gmail.com>
2026-01-08 21:50:03 +0530

    completed assignment 2

--- assign2/server.ts
+++ assign2/server.ts
@@ -1,3 +1,6 @@
+import {v4 as uuidv4} from 'uuid'
+import { SchemaReq } from "./types";
+
 const server = Bun.serve({
     port:3000,
     routes:{
@@ -5,7 +8,14 @@ const server = Bun.serve({
             GET : ()=>  Response.json({"status":"ok","timestamp":new Date},{status:200})
         },
         "/users":{
-            POST : ()=>
+            POST : async req=>{
+                const body = SchemaReq.safeParse(await req.body?.json());
+                if(!body.success){
+                    return Response.json({"error":"Validation error message"},{status:400});
+                }
+                const id = uuidv4();
+                return Response.json({"id":id,"name":body.data.name,"email":body.data.email,"age":body.data.age},{status:201});
+            }
         }
     }
 });
